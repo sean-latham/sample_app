0f99e268176ace1388e2b53929e6a1fc910d9996
Sean Latham <sean-latham@utulsa.edu>
2011-04-19 23:14:34 -0500

    Red test for header titles

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,11 +3,16 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
-
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+  it "should have correct title" do
+    get 'home'
+    response.should have_selector("title",
+                                   :content => "Rails Sample App | Home")
     end
   end
 
@@ -16,12 +21,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+   
+    it "should have correct title" do
+      get 'contact'
+      response.should have_selector("title",
+                                     :content => "Rails Sample App | Contact")
+      end
   end
 
 describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+      end
+
+    it "should have correct title" do
+      get 'about'
+      response.should have_selector("title", 
+                                     :content => "Rails Sample App | About")
       end
     end
 
